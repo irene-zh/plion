dca6b59537dafb19cf96600c1afcf7786ad71462
Irene Zhang <irenezy.zhang@gmail.com>
2018-08-29 23:28:29 -0400

    converted basic HTML site to jekyll

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.navbar-collapse a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+});
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar").addClass("top-nav-collapse");
+    } else {
+        $(".navbar").removeClass("top-nav-collapse");
+    }
+});
